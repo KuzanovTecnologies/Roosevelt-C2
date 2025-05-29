b046acde663e0afd55d3794ec10deb35b5dc6fa8
KuzanovTecnologies <diegorego514@gmail.com>
2025-05-29 11:46:23 -0300

    Add files via upload

---
+++ Roosevelt-C2.py
@@ -0,0 +1,97 @@
+#!/usr/bin/src/python3
+
+import requests
+import sys
+import os
+
+#--coding --UTF-8--
+
+import socket
+
+temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+temp.settimeout(7)
+
+host_ip = input('Target IP: 515.635.131.10')
+Target_Port_Number = input('Port you want to attack:4019')
+def system_attack(system_attack_options):
+    if temp.connect_ex((host_ip, port_number)):
+        print("You have been HACKED, Your Computer/System is COMPROMISED!\n");
+    else:
+      print("Error: Attack Failed, Failed To Hack!");
+
+#!/usr/bin/src/python3
+import requests
+import sys
+import os 
+
+# --Coding --UTF-8--
+
+import socket
+
+host_ip = input('Target_IP: 515.635.131.10') 
+temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+temp.settimeout(7);
+
+ 
+system_control = input(('host_ip, port_number'));
+
+system_control = input('Attack_the_host_IP/Port_Number');
+
+system_control = input('Bypass_Antivirus && Compromise the Firewall');
+
+system_control_panel_C2 = input('Hijack_The_Victim_Machine');
+
+system_control_panel_C2 = input('Destroys_The_Victim_Machine');
+
+#!/usr/bin/src/python3
+
+import requests 
+import sys
+import os 
+
+# --Coding --UTF-8--
+
+import socket
+
+host_ip = input('Target_IP: 515.635.131.10');
+def system_security(system_security_options):
+    if full.connect_ex((host_ip, port_number)):
+      print("Your Computer/System is safe, CONGRATULATIONS!");
+    if():
+      print("Your Computer/System is destroyed, Mission Failed!");
+
+# implement attack_modules
+
+host_ip = input('Target_IP: 515.635.131.10');
+def Target_Attack(Target_Attack_Options):
+    if full.connect_ex((host_ip, port_number)):
+      print("Target Computer has been hacked, CONGRATULATIONS!");
+    if():
+      print("Your Computer has been compromised, Mission Failed");
+
+# implement defense_modules
+
+host_ip = input('Target_IP: 515.635.131.10');
+def Target_Defense(Target_Defense_Options):
+  if full.connect_ex((host_ip, port_number)):
+    print("Victim Computer has been secured, CONGRATULATIONS!");
+  if():
+    print("Victim Computer had been compromised, Too Bad, Try again?");
+
+# implement pentesting_modules
+
+host_ip = input('Target_IP: 515.635.131.10');
+def Pentesting_Defense(Victim_Pentesting_Options):
+  if full.connect_ex((victim_ip, pentester_ip)):
+    print("Victim Computer is safe, CONGRATULATIONS!");
+  if():
+    print("Victim Computer is compromised, Too bad, Try Again?");
+
+# implement hacking_modules
+
+host_ip = input('Target_IP: 535.635.131.10');
+def Hacking_Defense(Victim_Hacking_Options):
+  if full.connect_ex((victim_ip, hacker_ip)):
+    print("Victim Computer is compromised, Too Bad, Try Again?");
+  if():
+    print("Victim Computer is safe, CONGRATULATIONS!");
