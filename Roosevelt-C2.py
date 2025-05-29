c5ddf42ce6d007e1f02c1941a74fff4a814f8e92
KuzanovTecnologies <diegorego514@gmail.com>
2025-05-29 13:44:09 -0300

    Update Roosevelt-C2.py

--- Roosevelt-C2.py
+++ Roosevelt-C2.py
@@ -11,8 +11,8 @@ import socket
 temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 temp.settimeout(7)
 
-host_ip = input('Target IP: 515.635.131.10')
-Target_Port_Number = input('Port you want to attack:4019')
+host_ip = input('Target IP: 230.156.257.289');
+Target_Port_Number = input('Port you want to attack: 3580');
 def system_attack(system_attack_options):
     if temp.connect_ex((host_ip, port_number)):
         print("You have been HACKED, Your Computer/System is COMPROMISED!\n");
@@ -28,11 +28,10 @@ import os
 
 import socket
 
-host_ip = input('Target_IP: 515.635.131.10') 
+host_ip = input('Target_IP: 212.189.258.320');
 temp = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 temp.settimeout(7);
 
- 
 system_control = input(('host_ip, port_number'));
 
 system_control = input('Attack_the_host_IP/Port_Number');
@@ -53,7 +52,7 @@ import os
 
 import socket
 
-host_ip = input('Target_IP: 515.635.131.10');
+host_ip = input('Target_IP: 69.87.142.185');
 def system_security(system_security_options):
     if full.connect_ex((host_ip, port_number)):
       print("Your Computer/System is safe, CONGRATULATIONS!");
@@ -62,7 +61,7 @@ def system_security(system_security_options):
 
 # implement attack_modules
 
-host_ip = input('Target_IP: 515.635.131.10');
+host_ip = input('Target_IP: 47.62.88.115');
 def Target_Attack(Target_Attack_Options):
     if full.connect_ex((host_ip, port_number)):
       print("Target Computer has been hacked, CONGRATULATIONS!");
@@ -71,7 +70,7 @@ def Target_Attack(Target_Attack_Options):
 
 # implement defense_modules
 
-host_ip = input('Target_IP: 515.635.131.10');
+host_ip = input('Target_IP: 140.180.270.360');
 def Target_Defense(Target_Defense_Options):
   if full.connect_ex((host_ip, port_number)):
     print("Victim Computer has been secured, CONGRATULATIONS!");
@@ -80,7 +79,7 @@ def Target_Defense(Target_Defense_Options):
 
 # implement pentesting_modules
 
-host_ip = input('Target_IP: 515.635.131.10');
+host_ip = input('Target_IP: 140.190.237.180');
 def Pentesting_Defense(Victim_Pentesting_Options):
   if full.connect_ex((victim_ip, pentester_ip)):
     print("Victim Computer is safe, CONGRATULATIONS!");
@@ -89,9 +88,18 @@ def Pentesting_Defense(Victim_Pentesting_Options):
 
 # implement hacking_modules
 
-host_ip = input('Target_IP: 535.635.131.10');
+host_ip = input('Target_IP: 87.147.217.140');
 def Hacking_Defense(Victim_Hacking_Options):
   if full.connect_ex((victim_ip, hacker_ip)):
     print("Victim Computer is compromised, Too Bad, Try Again?");
   if():
     print("Victim Computer is safe, CONGRATULATIONS!");
+
+
+# implement attacker_modules
+host_ip = ('Target_IP: 126.87.180.67');
+def Hijacking_Victim_Defenses(Victim_Hijacking_Options):
+  if full.connect_ex((victim_ip, attacker_ip)):
+    print("Victim Machine/System is COMPROMISED, CONGRATULATIONS!");
+  if():
+    print("Victim Computer/System is Secure, Too bad, Try again?");
